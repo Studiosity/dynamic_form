0b763b22e942bdea6227e49fa980a2941dffb4f4
Andrew Bromwich <a.bromwich@gmail.com>
2021-06-18 13:08:37 +1000

    Update dynamic errors to address Rails 6.1 deprecation warning

--- lib/active_model/dynamic_errors.rb
+++ lib/active_model/dynamic_errors.rb
@@ -11,34 +11,25 @@ module ActiveModel
     #     which gives an error message like:
     #     Rate (Accuracy) is required
     def full_messages
-      full_messages = []
-
-      each do |attribute, messages|
-        messages = Array.wrap(messages)
-        next if messages.empty?
-
-        if attribute == :base
-          messages.each {|m| full_messages << m }
+      map do |error|
+        if error.attribute == :base
+          error.message
         else
-          attr_name = attribute.to_s.gsub('.', '_').humanize
-          attr_name = @base.class.human_attribute_name(attribute, :default => attr_name)
-          options = { :default => "%{attribute} %{message}", :attribute => attr_name }
+          attr_name = error.attribute.to_s.gsub('.', '_').humanize
+          attr_name = @base.class.human_attribute_name(error.attribute, default: attr_name)
+          options = { default: "%{attribute} %{message}", attribute: attr_name }
 
-          messages.each do |m|
-            if m =~ /^\^/
-              options[:default] = "%{message}"
-              full_messages << I18n.t(:"errors.dynamic_format", options.merge(:message => m[1..-1]))
-            elsif m.is_a? Proc
-              options[:default] = "%{message}"
-              full_messages << I18n.t(:"errors.dynamic_format", options.merge(:message => m.call(@base)))
-            else
-              full_messages << I18n.t(:"errors.format", options.merge(:message => m))
-            end            
+          if error.message =~ /^\^/
+            options[:default] = "%{message}"
+            I18n.t(:"errors.dynamic_format", options.merge(message: error.message[1..-1]))
+          elsif error.message.is_a? Proc
+            options[:default] = "%{message}"
+            I18n.t(:"errors.dynamic_format", options.merge(message: error.message.call(@base)))
+          else
+            I18n.t(:"errors.format", options.merge(message: error.message))
           end
         end
       end
-
-      full_messages
     end
   end
 end
